421e124d035c80c0e2498a58c62eb416ffbc6a14
Rahul Sharma <rahulsharma94595@gmail.com>
2023-01-10 14:59:09 +0530

    pattern11

---
+++ pattern11.js
@@ -0,0 +1,14 @@
+((value) => {
+  let str = "";
+  for (let i = value; i >= 1; i--) {
+    for (let j = 1; j <= 2 * value - 1; j++) {
+      if (j >= value - (i - 1) && j <= value + (i - 1)) {
+        str += "*";
+      } else {
+        str += " ";
+      }
+    }
+    str += "\n";
+  }
+  console.log(str);
+})(9);
